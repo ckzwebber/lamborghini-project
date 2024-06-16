12c6ccb562f9285483abc6f821fe3c998ad4bd49
Carlos Miguel <85066001+ckzwebber@users.noreply.github.com>
2024-06-16 11:43:24 -0300

    Working

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,25 @@
-import Slider from "./Slider";
+import img from "../assets/images/about.jpg";
+import "./About.css";
 
-export default function Models() {}
+export default function About() {
+  return (
+    <div className="About">
+      <div className="background-image"></div>
+      <div className="content">
+        <p>
+          About Us Unleashing the Power of Excellence Since 1963, Lamborghini
+          has been dedicated to pushing the boundaries of automotive performance
+          and design. Founded by Ferruccio Lamborghini in Sant'Agata Bolognese,
+          Italy, we have become synonymous with luxury, power, and
+          craftsmanship. Our Philosophy At Lamborghini, we create more than just
+          cars; we craft experiences. Each vehicle is a masterpiece, combining
+          cutting-edge technology with stunning design to deliver an
+          unforgettable driving experience. Pioneering the Future We lead with
+          innovation, from hybrid powertrains to advanced aerodynamics,
+          constantly redefining high-performance automobiles. Join the
+          Lamborghini family and be part of a legacy of excellence.
+        </p>
+      </div>
+    </div>
+  );
+}
