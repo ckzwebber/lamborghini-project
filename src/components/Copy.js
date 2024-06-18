dc34ecf53cfc77cb98608535aa6eb6126ae35b92
Carlos Miguel <85066001+ckzwebber@users.noreply.github.com>
2024-06-18 15:40:47 -0300

    Working

--- src/components/Copy.js
+++ src/components/Copy.js
@@ -1,10 +1,7 @@
 export default function Copy() {
   return (
     <div className="copy">
-      <p>
-        This website is just a study project and is not official from
-        Lamborghini. All images, brands and names are property of Lamborghini.
-      </p>
+      <p>This website is not official from Lamborghini.</p>
     </div>
   );
 }
