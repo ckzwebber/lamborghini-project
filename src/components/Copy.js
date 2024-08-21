417bcabfd6c14e8bfe0f4e94f6096eadaf99bd39
Carlos Miguel <85066001+ckzwebber@users.noreply.github.com>
2024-08-21 16:06:27 -0300

    Add contacts

--- src/components/Copy.js
+++ src/components/Copy.js
@@ -1,9 +1,17 @@
 import "../styles/App.css"
+import gitHub from "../assets/images/github-logo.png"
+import linkedin from "../assets/images/linkedin.png"
 
 export default function Copy() {
   return (
-    <footer>
-      <p>This website is not official from Lamborghini.</p>
-    </footer>
+      <footer>
+          <p>This website is not official from Lamborghini.</p>
+          <a href="https://github.com/ckzwebber" target="_blank" rel="noopener noreferrer">
+              <img className="social" src={gitHub} alt="GitHub logo"/>
+          </a>
+          <a href="https://www.linkedin.com/in/cmiguelwm/" target="_blank" rel="noopener noreferrer">
+              <img className="social" src={linkedin} alt="Linkedin"/>
+          </a>
+      </footer>
   );
 }
